04d8bafc0cec7cd75896789ec9e2a1c5929eae49
Fabio Albuquerque <albuquerquefabio@icloud.com>
2021-06-29 23:40:58 -0300

    Babel and Jest

---
+++ babel.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+          esmodules: true
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@auth': './src/auth',
+          '@controllers': './src/api/controllers',
+          '@env': './src/environment',
+          '@lib': './src/lib',
+          '@models': './src/api/models',
+          '@routers': './src/api/routers',
+          '@types': './src/@types'
+        }
+      }
+    ]
+  ],
+  ignore: ['**/*.spec.ts']
+}
