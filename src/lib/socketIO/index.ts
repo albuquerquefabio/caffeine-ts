fb142ca51ea2f5e53559542179b18ba8956631e0
Fabio Albuquerque <albuquerquefabio@icloud.com>
2021-07-01 02:51:58 -0300

    Fix socket condition on disconnect

--- src/lib/socketIO/index.ts
+++ src/lib/socketIO/index.ts
@@ -36,8 +36,8 @@ const conn = async (io: Server, socket: Socket): Promise<void> => {
   // save user on Redis
   if (_user.length) await saveSocketUserId(_user, socket.id)
   socket.on('disconnect', async () => {
-    if (!environment.log) return
     if (_user.length) await removeSocketUserId(_user, socket.id)
+    if (!environment.log) return
     const count = io.of('/').sockets.size
     log.info('Socket-> disconnect')
     log.info(`Socket-> total clients`, count)
